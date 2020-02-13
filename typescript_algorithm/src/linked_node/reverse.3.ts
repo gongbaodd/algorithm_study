56eff7e0adbdfe8a06f42bf67251deaad55d5426
gongbaodd@Inspiron <gongbaodd@qq.com>
2020-02-13 18:18:01 +0800

    reverse.3

---
+++ typescript_algorithm/src/linked_node/reverse.3.ts
@@ -0,0 +1,17 @@
+import {LNode, result} from './lib';
+
+result(head => {
+    let arr = [];
+    
+    let curr = head.next;
+    while(curr) {
+        arr.push(curr);
+        curr = curr.next;
+    }
+
+    for(let i = arr.length; i > 0; i--) {
+        arr[i-1].next = arr[i-2] || null;
+    }
+
+    head.next = arr[arr.length - 1];
+});
