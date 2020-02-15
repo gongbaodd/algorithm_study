0bd6f44e0f69eb69c3020355cfde3e9b78c81feb
gongbaodd@InspironWSL <gongbaodd@qq.com>
2020-02-15 17:33:27 +0800

    ✨ Feat: find last k

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@ module.exports = {
   preset: "ts-jest",
   clearMocks: true,
   collectCoverage: true,
-  collectCoverageFrom: ["src/**"],
   coverageDirectory: "coverage",
   coverageReporters: ["json", "lcov", "text-summary", "clover"],
-  testEnvironment: "node"
+  testEnvironment: "node",
+  rootDir: "src"
 };
